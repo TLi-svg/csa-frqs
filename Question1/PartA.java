a9a0161943315abe54bafaada47b8c9be062eeb7
TLi-svg <timothyl86@nycstudents.net>
2025-05-13 11:20:16 -0400

    Finalize PartA

--- Question1/PartA.java
+++ Question1/PartA.java
@@ -1,3 +1,11 @@
-public int walkDogs(int hour) {
-  
-}
+    public int walkDogs(int hour) {
+        int avail = company.numAvailableDogs();
+
+        if (maxDogs > avail) {
+            updateDogs(hour, avail);
+            return avail;
+        } else {
+            updateDogs(hour, maxDogs);
+            return maxDogs;
+        }
+    }
