3ad70d54bef93e9a4d69ceecbf14e1f0b5a76118
TLi-svg <timothyl86@nycstudents.net>
2025-05-15 11:53:49 -0400

    Update PartB.java
    fixed the for loop conditions to account for half length

--- Question3/PartB.java
+++ Question3/PartB.java
@@ -2,11 +2,11 @@
         ArrayList<Match> built = new ArrayList<Match>();
 
         if (competitorList.size() % 2 == 0) {
-            for (int i = 0; i < competitorList.size(); i++) {
+            for (int i = 0; i < competitorList.size() / 2; i++) {
                 built.add(new Match(competitorList.get(i), competitorList.get(competitorList.size() - 1 - i)));
             }
         } else {
-            for (int i = 1; i < competitorList.size(); i++) {
+            for (int i = 1; i < (competitorList.size()) - 1 / 2; i++) {
                 built.add(new Match(competitorList.get(i), competitorList.get(competitorList.size() - 1 - i)));
             }
         }
